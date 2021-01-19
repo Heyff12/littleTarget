eda4409c55cb663478e50f702f814500070c697c
Heyff12 <645068564@qq.com>
2021-01-19 16:53:13 +0800

    canvas

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
-import Taro from '@tarojs/taro'
+import Taro,{NodesRef} from '@tarojs/taro'
 import { connect } from 'react-redux'
-import { View, Picker } from '@tarojs/components'
+import { View, Picker,Canvas } from '@tarojs/components'
 import { AtButton } from 'taro-ui'
 import { fetchTarget, fetchAllTarget, saveTarget } from '../../actions/target'
 import {CategoryEnum,quarterValue} from '../../constants/actions'
 import EmptyTarget from '../../components/emptyTarget'
 import {INITIAL_STATE} from '../../reducers/target'
-import Painter from 'mina-painter'
-import Card from "../../palette/card";
 import './index.less'
+import imgUrl from '../../assets/img/book.jpg'
 
 
 type PageStateProps = {
@@ -29,7 +28,6 @@ type PageState = {
   selectorChecked:number,
   targets: Target.TargetItem[],
   imagePath: string,
-  template: any
 }
 
 type IProps = PageStateProps & PageDispatchProps & PageOwnProps & PageState
@@ -56,12 +54,13 @@ class Index extends Component {
   tableImage = React.createRef()
   // tableImage = (node) => this.tableImage = node
 
+  canvas:any
+
   state = {
     selector: [2020,2021,2022],
     selectorChecked: 2020,
     targets:[],
-    imagePath: "",
-    template: new Card().palette()
+    imagePath: ""
   }
 
   componentWillReceiveProps (nextProps) {
@@ -70,6 +69,10 @@ class Index extends Component {
   componentDidMount () {
     this.setYearRange()
     this.getData()
+    setTimeout(() => {
+      this.buildImage()
+
+    }, 100);
   }
 
   getData = () => {
@@ -113,8 +116,164 @@ class Index extends Component {
     })
   }
 
-  buildImage = () => {
-    this.saveImage()
+  exportImage = () => {
+    Taro.canvasToTempFilePath({
+      canvasId: '#canvas',
+      canvas: this.canvas,
+      success(res) {
+        Taro.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success(saveRes) {
+            console.log(saveRes, 'saveRes')
+          }
+        })
+      },
+      fail(fail) {
+        console.log(fail)
+      }
+    })
+  }
+
+  buildImage =()=> {
+    // this.saveImage()
+    // 创建一个选择器对象
+    const query = Taro.createSelectorQuery()
+    // 选择当前页面下第一个 #canvas
+    // 返回 NodesRef 用于获取 WXML 节点信息的对象
+    query.select('#canvas')
+      // 获取节点的相关信息。需要获取的字段在fields中指定。返回值是 nodesRef 对应的 selectorQuery
+      .fields({ node: true, size: true })
+      // 执行所有的请求。请求结果按请求次序构成数组，在callback的第一个参数中返回
+      .exec(this.draw.bind(this))
+  }
+
+  draw (res: NodesRef) {
+    console.log(res)
+    // Canvas 实例
+    const canvas = res[0].node
+    this.canvas = canvas
+    // Canvas 的绘图上下文
+    const ctx = canvas.getContext('2d')
+
+    // 设备像素比
+    // 这里根据像素比来设置 canvas 大小
+    const dpr = Taro.getSystemInfoSync().pixelRatio
+    canvas.width = res[0].width * dpr
+    canvas.height = res[0].height * dpr
+    ctx.scale(dpr, dpr)
+
+   // 解决下载来的图片是黑色背景的问题
+    ctx.fillStyle = 'rgba(0,0,0,0.1)'
+    ctx.fillRect(0, 0, res[0].width, res[0].height)
+
+    ctx.fillRect(125, 25, 100, 100); // 画实心矩形
+    ctx.clearRect(145, 45, 60, 60); // 清除矩形区域
+    ctx.strokeRect(150, 50, 50, 50); // 画矩形线框
+
+    ctx.fillStyle = 'green'
+    ctx.beginPath();
+    ctx.moveTo(5, 20);
+    ctx.lineTo(20, 45);
+    ctx.lineTo(20, 5);
+    ctx.fill();
+
+    ctx.strokeStyle = 'orange'
+    ctx.lineWidth = 2
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // 绘制
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false);   // 口(顺时针)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // 左眼
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // 右眼
+    ctx.closePath()
+    ctx.stroke();
+
+    // 画背景
+    ctx.fillStyle = '#FD0';
+    ctx.fillRect(264,0,75,75);
+    ctx.fillStyle = '#6C0';
+    ctx.fillRect(339,0,75,75);
+    ctx.fillStyle = '#09F';
+    ctx.fillRect(264,75,75,75);
+    ctx.fillStyle = '#F30';
+    ctx.fillRect(339,75,75,75);
+    ctx.fillStyle = '#FFF';
+
+    // 设置透明度值
+    ctx.globalAlpha = 0.2;
+
+    // 画半透明圆
+    for (var i=0;i<7;i++){
+        ctx.beginPath();
+        ctx.arc(339,75,10+10*i,0,Math.PI*2,true);
+        ctx.fill();
+    }
+
+    // 创建新 image 对象，用作图案
+    var img1 = new Image();
+    img1.src = imgUrl
+    img1.onload = function() {
+      // 创建图案
+      ctx.drawImage(img1,0,0);
+      var ptrn = ctx.createPattern(img1, 'repeat');
+      ctx.fillStyle = ptrn;
+      ctx.fillRect(0, 0, 150, 150);
+    }
+
+    // 获取图片信息
+    // Taro.getImageInfo({
+    //   src: '../../assets/img/book.jpg',
+    //   success: resImg => {
+    //     // 创建一个图片对象
+    //     const img = canvas.createImage();
+
+    //     img.src = '../../' + resImg.path
+    //     img.onload = () => {
+    //       // 绘制图像到画布
+    //       ctx.drawImage(img, 0, 0, 300, 150)
+    //     }
+
+
+    //   },
+    //   fail(fail) {
+    //     console.log(fail)
+    //   }
+    // })
+    ctx.globalAlpha = 1;
+    // 绘制文本
+    ctx.font = "16px Times New Roman";
+    ctx.fillStyle = 'pink'
+    ctx.fillText('英语直播', 20, 16)
+
+    // 同上面的功能
+    const img = canvas.createImage();
+
+    img.src = imgUrl
+    img.onload = () => {
+      ctx.drawImage(img, 20, 200, 70, 70)
+    }
+
+    // 绘制文本
+    ctx.font = "14px Times New Roman";
+    ctx.fillStyle = 'blue'
+    ctx.fillText('@Owen', 20, 40)
+
+    // 绘制文本
+    ctx.font = '12px sans-serif'
+    ctx.fillStyle = 'black'
+    ctx.fillText('给你推荐一堂好课', 20, 60)
+
+    // 绘制文本
+    ctx.font = '12px sans-serif'
+    ctx.fillStyle = 'black'
+    ctx.fillText('长按识别小程序查看课程', 20, 80)
+
+    // 绘制文本
+    ctx.font = '10px sans-serif'
+    ctx.fillStyle = 'gary'
+    ctx.fillText('来自：奋翔升学服务中心', 20, 100)
   }
 
   onImgOK = (path) => {
@@ -181,8 +340,9 @@ class Index extends Component {
     return endDom
   }
 
+
   render () {
-    const { selector,selectorChecked ,template} = this.state
+    const { selector,selectorChecked} = this.state
     return (
         <View className='index'>
           <View className="fixedTop">
@@ -195,7 +355,7 @@ class Index extends Component {
               </Picker>
             </View>
             <View>
-              <AtButton type='primary' size='small' onClick={this.buildImage}>导出图片</AtButton>
+              <AtButton type='primary' size='small' onClick={this.exportImage}>导出图片</AtButton>
             </View>
           </View>
           <View className="mainSection" ref={this.tableImage}>
@@ -214,10 +374,11 @@ class Index extends Component {
             </View>
           </View>
           {/* <wxml-to-canvas class="widget"></wxml-to-canvas> */}
-          <Painter
+          {/* <Painter
             customStyle='margin-left:40rpx'
             palette={template}
-            onImgOK={this.onImgOK} />
+            onImgOK={this.onImgOK} /> */}
+          <Canvas id='canvas' className='canvas' type='2d' />
         </View>
     )
   }
